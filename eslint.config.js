bda66f8ba7545b869a2e5c97b3ea49da269a6322
Garrett Corbin <jgarrettcorbin@gmail.com>
2024-11-24 18:42:51 -0800

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -19,10 +19,25 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "no-unused-vars": ["off"],
+      "@typescript-eslint/no-unused-vars": ["warn",
+        {
+          argsIgnorePattern: '^_', // Ignore arguments that start with an underscore (standard practice)
+          ignoreRestSiblings: true, // Ignore unused rest siblings
+          args: 'after-used', // Only flag unused arguments that are never used
+          varsIgnorePattern: '^.*$', // Don't apply any pattern rules for unused variable names
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": ["warn"],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+    
     },
   },
 )
