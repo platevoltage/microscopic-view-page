f8a0b6bc676e9862cccd8d7386f5cdbb228a1b74
Garrett Corbin <jgarrettcorbin@gmail.com>
2024-11-26 20:51:31 -0800

    added time limit on demo

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,7 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Demo() {
   const [ isRunning, setIsRunning ] = useState(false);
+  const [ secondsLeft, setSecondsLeft ] = useState(0);
+
+  useEffect(() => {
+    let timeout: number | undefined;
+    const timeStart = new Date().getTime() + 20000;
+    setSecondsLeft(timeStart);
+    if (isRunning) {
+      timeout = setInterval(() => {
+        // timeStart -= 1000;
+        const currentTime = new Date().getTime();
+        setSecondsLeft(Math.floor((timeStart - currentTime)/1000));
+        console.log({currentTime, timeStart});
+        console.log(timeStart - currentTime, timeStart < currentTime);
+        if (timeStart < currentTime) {
+          setIsRunning(false);
+        }
+      }, 1000);
+    } else {
+      clearInterval(timeout);
+    }
+
+    // if (isRunning) {
+    //   timeout = setTimeout(() => {
+    //     setIsRunning(false);
+    //   }, 60000);
+    // } else {
+    //   clearTimeout(timeout);
+    // }
+    return () => {
+      clearInterval(timeout);
+    };
+  }, [isRunning]);
 
   return (
     <>
@@ -11,6 +43,7 @@ export default function Demo() {
           <div style={{width: "860px", height: isRunning ? "500px" : "100px", position: "relative", borderRadius: "10px", transition: ".4s height", overflow: "hidden"}}>
             <iframe src="/demo/index.html" style={{ width: "100%", height: "100%", borderRadius: "10px", border: "1px solid #ffffff22", boxShadow: "1px 2px 10px black"}}></iframe>
             <button style={{border: "0px solid white", padding: "5px 10px", position: "absolute", left: 5, top: 5, width: "60px", fontSize: ".8em"}} onClick={() => setIsRunning(false)}>Close</button>
+            <button style={{border: "0px solid white", padding: "5px 10px", position: "absolute", right: 5, top: 5, width: "160px", fontSize: ".8em"}} onClick={() => setIsRunning(false)}>{secondsLeft} seconds left</button>
           </div>
           :
           <div style={{width: "860px", borderRadius: "10px", backgroundColor: "#000000", display: "flex", justifyContent: "center", border: "1px solid #ffffff22", boxShadow: "1px 2px 10px black"}}>
